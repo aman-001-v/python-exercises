7f104026a205ca7e493c7305f6a53291978b0696
aman-001-v <a0110verma@gmail.com>
2026-07-04 18:25:45 +0530

    chapter 7 over

---
+++ multipleOfTen.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a number: "))
+if n % 10 == 0:
+    print(f"{n} is a multiple of 10.")
+else:
+    print(f"{n} is not a multiple of 10")
---
+++ restaurantSeating.py
@@ -0,0 +1,5 @@
+n = int(input("How many people are there in your dinner group???"))
+if n > 8:
+    print("You guys have to wait for a table.")
+else:
+    print("Your table is ready!!!")
