751e8d60fe523a9cbcbf2ee815770687c53e72eb
aman-001-v <a0110verma@gmail.com>
2026-07-06 19:02:43 +0530

    context manager still weak

---
+++ contextmanager1.py
@@ -0,0 +1,14 @@
+class database():
+    def __init__(self):
+        print("Database connected.")
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self , exc_type , exc_val , exc_tb):
+        print("Database Closed.")
+    def execute(self , qry):
+        print(qry)
+with database() as db:
+    db.execute("harsh")
+
